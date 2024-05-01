309375a9471686feb7ca36f08c186fb36fe443b8
Christian.hub <158318240+Christian-hub-online@users.noreply.github.com>
2024-05-01 12:22:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(
+    setTimeout(function(){
+    $('.spinner').addClass('hidden');
+    $('main').removeClass('hidden');
+    $('header').removeClass('hidden');
+    $('.background').removeClass('hidden');
+}, 2000));
