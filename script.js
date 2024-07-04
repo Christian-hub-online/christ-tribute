fd9f6e3a8ac38654901e57b727dc034b16d34a68
Christian.hub <158318240+Christian-hub-online@users.noreply.github.com>
2024-07-04 12:41:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+/* John 14:6 “I am the way and the truth and the life. No one comes to the Father except through me.*/
 $(document).ready(
     setTimeout(function(){
     $('.spinner').addClass('hidden');
     $('main').removeClass('hidden');
     $('header').removeClass('hidden');
     $('.background').removeClass('hidden');
-}, 5000));
+}, 2000));
